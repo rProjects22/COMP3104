20c268f779c333eb6b2b09b99bb9d43e02824324
rProjects22 <rProjects@gmail.com>
2022-10-11 13:16:36 -0400

    React project code added

---
+++ build/precache-manifest.f4290d69944edd3e803da19d5b7538bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "447f8d33dfdc5cef27ba",
+    "url": "/rProjects22/COMP3104/static/js/runtime~main.447f8d33.js"
+  },
+  {
+    "revision": "02347b3d4039c462e903",
+    "url": "/rProjects22/COMP3104/static/js/main.02347b3d.chunk.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/rProjects22/COMP3104/static/js/2.b1773f8a.chunk.js"
+  },
+  {
+    "revision": "02347b3d4039c462e903",
+    "url": "/rProjects22/COMP3104/static/css/main.d23220b3.chunk.css"
+  },
+  {
+    "revision": "dcad014af52af1081d2a66f4379cf3fb",
+    "url": "/rProjects22/COMP3104/index.html"
+  }
+];
